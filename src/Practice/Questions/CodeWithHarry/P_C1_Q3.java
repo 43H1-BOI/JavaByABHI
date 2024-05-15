3cc3f88b1370f3d6e4b4d084237af9e519234fee
43H1-BOI <me.abhiyadav69@gmail.com>
2024-05-15 21:28:05 +0530

    Updated Text File

---
+++ src/Practice/Questions/CodeWithHarry/P_C1_Q3.java
@@ -0,0 +1,11 @@
+package Practice.Questions.CodeWithHarry;
+
+import java.util.Scanner;
+public class P_C1_Q3 {
+    public static void main(String[] args){
+        Scanner Sc = new Scanner(System.in);
+        System.out.print("Enter Your Name : ");
+        String Name = Sc.next();
+        System.out.print("Hello "+Name+"! Good Morning Ji");
+    }
+}
