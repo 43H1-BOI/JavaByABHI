abc300a7e9ebfe9efd8ed928437d1dde0131e4a9
43H1-BOI <me.abhiyadav69@gmail.com>
2024-06-28 18:47:44 +0530

    Brute Forced code

--- src/Practice/Arrays_6/PartitionSumIn.java
+++ src/Practice/Arrays_6/PartitionSumIn.java
@@ -26,33 +26,72 @@ public class PartitionSumIn {
         }
     }
 
-    public static void prefixSum(int[] arr) {
+    public static boolean partitionSum(int[] arr) {
+        boolean cond = false; //main condition
+        int part = 1; // partition
+        int n = arr.length; // length of array
 
+        while (part < n) { // partition < size of array
+            int pSum = 0; // prefix Sum
+            int sSum = 0; // suffix Sum
+            int j = 0; // temp variable
 
+            while (j < part) {
+                pSum += arr[j];
+                j++;
+            }
+            System.out.println("p" + pSum);
+
+            int i = part;
+            while (i < n) {
+                sSum += arr[i];
+                i++;
+            }
+            System.out.println("s" + sSum);
+            part++;
+
+            if (pSum == sSum) {
+                cond = true;
+                System.out.println("Index of i = " + part);
+                return cond;
+            }
+            float avg = (float) ((pSum + sSum) % 2);
+            if (avg != 0) {
+                return cond;
+            }
+
+        }
+
+        return cond;
     }
 
     public static void main(String[] args) {
-        int[] arr = getArr();
-        System.out.println("Elements in Array are : ");
-        printArr(arr);
+        int[] arr = new int[]{5, 3, 2, 6, 3, 1};
+//        int[] arr = getArr();
+//        System.out.println("Elements in Array are : ");
+//        printArr(arr);
+        System.out.println(partitionSum(arr));
 
-        int preSum = 0;
-        int sufSum = 0;
 
-        int i = 0;
-        int j = (arr.length - 1);
-        while (i >= j) {
-            if (preSum != sufSum && i >= j) {
-                preSum += arr[i];
-                i++;
-            }
-            sufSum += arr[j];
-            j--;
-
-            if (preSum == sufSum && i >= j) {
-                break;
-            }
-        }
-
+//        {
+//
+//        int preSum = 0;
+//        int sufSum = 0;
+//        int i = 0;
+//        int j = (arr.length - 1);
+//        while (i >= j) {
+//            if (preSum != sufSum && i >= j) {
+//                preSum += arr[i];
+//                i++;
+//            }
+//            sufSum += arr[j];
+//            j--;
+//
+//            if (preSum == sufSum && i >= j) {
+//                break;
+//            }
+//        }
+//
+//    }
     }
 }
