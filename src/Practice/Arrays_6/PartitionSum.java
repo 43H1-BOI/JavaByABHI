1502f3f8bada3bf6b25ac82a90db72da86931706
43H1-BOI <me.abhiyadav69@gmail.com>
2024-06-28 20:03:37 +0530

    Optimized Code for Better Performance (Last Time)

--- src/Practice/Arrays_6/PartitionSum.java
+++ src/Practice/Arrays_6/PartitionSum.java
@@ -79,33 +79,103 @@ public class PartitionSum {
         return cond; // means no partition found
     }
 
+    public static void partSum2(int[] arr) {
+        int totalSum = totalSum(arr);
+        int part = 1;
+
+        while (part < arr.length) {
+            int preSum = 0;
+            for (int i = part; i >= 0; i--) {
+                preSum += arr[i];
+            }
+
+            int suffSum = totalSum - preSum;
+
+            // This Condition will be Checked only once to save our resources
+            if (part == 1) {
+                // This Condition means if Partition Sum Exists for any array then
+                // it will not follow below condition
+                int avg = (preSum + suffSum) % 2;
+                if (avg != 0) {
+                    System.out.println("Sorry Mitr !! ");
+                    break;
+                }
+                // This Condition just says if partition sum exists then sum of preSum and suffSum must be an even number
+            }
+
+
+            if (preSum == suffSum) {
+                System.out.println("Partition Sum Found !!");
+                System.out.println("Partition Index = " + part);
+                break;
+            }
+            part++;
+        }
+//        System.out.println("No Partition Sum Found !!");
+
+    }
+
+    // Most Efficient Method (I think)
+    public static void partSum3(int[] arr) {
+        int[] preSum = prefixSum(arr);
+        int totalSum = totalSum(arr);
+
+        for (int i = 1; i < arr.length; i++) {
+            int suffSum = totalSum - preSum[i];
+
+            // This Condition will be Checked only once to save our resources
+            if (i == 1) {
+                // This Condition means if Partition Sum Exists for any array then
+                // it will not follow below condition
+                int avg = (preSum[i] + suffSum) % 2;
+                if (avg != 0) {
+                    System.out.println("No Prefix Sum Found !");
+                    break;
+                }
+                // This Condition just says if partition sum exists then sum of preSum[i] and suffSum must be an even number
+            }
+
+
+            if (preSum[i] == suffSum) {
+                System.out.println("Partition Sum Found :)");
+                System.out.println("Partition Index = " + i);
+                break;
+            }
+        }
+    }
+
+    private static int totalSum(int[] arr) {
+        int total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            total += arr[i];
+        }
+        return total;
+    }
+
+    public static int[] prefixSum(int[] arr) {
+        int[] prefix = new int[arr.length];
+        prefix[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            prefix[i] = arr[i] + prefix[i - 1];
+        }
+        return prefix;
+    }
+
     public static void main(String[] args) {
-        int[] arr = new int[]{5, 3, 2, 6, 3, 1};
-//        int[] arr = getArr();
-//        System.out.println("Elements in Array are : ");
-//        printArr(arr);
+//        int[] arr = new int[]{5, 3, 2, 6, 3, 1};
+        System.out.println("Ex . { 5 , 3 , 2 , 6 , 3 , 1 }");
+        int[] arr = getArr();
+        System.out.println("Elements in Array are : ");
+        printArr(arr);
+
+        System.out.println("\n1st Method");
         System.out.println(partitionSum(arr));
 
+        System.out.println("\n2nd Method");
+        partSum2(arr);
 
-//        {
-//
-//        int preSum = 0;
-//        int sufSum = 0;
-//        int i = 0;
-//        int j = (arr.length - 1);
-//        while (i >= j) {
-//            if (preSum != sufSum && i >= j) {
-//                preSum += arr[i];
-//                i++;
-//            }
-//            sufSum += arr[j];
-//            j--;
-//
-//            if (preSum == sufSum && i >= j) {
-//                break;
-//            }
-//        }
-//
-//    }
+        System.out.println("\n3nd Method");
+        partSum3(arr);
+
     }
 }
