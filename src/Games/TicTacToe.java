6074ba69172dbccbd54e8060a2540b2cdc86e0ae
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2024-08-14 19:51:03 +0530

    Tic Tac Toe V1.1 testing

--- src/Games/TicTacToe.java
+++ src/Games/TicTacToe.java
@@ -5,16 +5,26 @@ import java.util.Scanner;
 public class TicTacToe {
     static Scanner Sc = new Scanner(System.in);
 
+    // Stores Successful Moves of game
+    static players[][] SuccessMoves = new players[3][3];
+
+    // Stores player 1 and player 2 info
+    static players p1, p2;
+
+    // Contains Players
     enum players {
-        X,
-        O;
+        Invalid, // Not a Valid Player
+        X, // Player X
+        O; // Player O
     }
 
-    static void Board(int[][] Moves, int cord1, int cord2) {
-
-        if (cord1 == cord2) {
-
-
+    static void Board(players[][] SuccessMoves, int cord1, int cord2) {
+        cord1--;
+        cord2--;
+        if (SuccessMoves[cord1][cord2] == players.Invalid) {
+            System.out.println("Invalid Move");
+        } else {
+            SuccessMoves[cord1][cord2] = p1;
         }
 
         /*
@@ -29,28 +39,28 @@ public class TicTacToe {
 
         */
 
-        System.out.println(" " + Moves[0][0] + " │ " + Moves[0][1] + " │ " + Moves[0][2] + " ");
+        System.out.println(" " + SuccessMoves[0][0] + " │ " + SuccessMoves[0][1] + " │ " + SuccessMoves[0][2] + " ");
         System.out.println("———¦———¦———");
-        System.out.println(" " + Moves[1][0] + " │ " + Moves[1][1] + " │ " + Moves[1][2] + " ");
+        System.out.println(" " + SuccessMoves[1][0] + " │ " + SuccessMoves[1][1] + " │ " + SuccessMoves[1][2] + " ");
         System.out.println("———¦———¦———");
-        System.out.println(" " + Moves[2][0] + " │ " + Moves[2][1] + " │ " + Moves[2][2] + " ");
+        System.out.println(" " + SuccessMoves[2][0] + " │ " + SuccessMoves[2][1] + " │ " + SuccessMoves[2][2] + " ");
 
 
+        /*
         // Typical X O Board
         System.out.println("    │    │   ");
         System.out.println("————¦————¦————");
         System.out.println("    │    │   ");
         System.out.println("————¦————¦————");
         System.out.println("    │    │   ");
+        */
 
     }
 
     public static void main(String[] args) {
         System.out.println("Tic Tac Toe Game : \n\n");
-        players p1, p2;
+//        players p1, p2;
         int choice = 0;
-        /*
-
 
         // For Selection of Player
         while (choice != 3) {
@@ -78,20 +88,21 @@ public class TicTacToe {
                     System.out.println("Invalid Choice !");
                     System.out.println("Please Select From Given Options : ");
             }
-            if(choice == 1 || choice == 2) break;
+            if (choice == 1 || choice == 2) break;
         }
 
         // To End Program if the user enters 3 ( To Exit Game )
-        if(choice == 3) return;
+        if (choice == 3) return;
 
         System.out.println("Let's Go Buddy !\n");
 
-*/
+
         // SuccessMoves counts the Successful Moves Entered by Player
         // If He/She Enters the coordinates where one player is already present then it will not be counted as successful move
         // SuccessMoves Array Contains the GameBoard
 
-        int[][] SuccessMoves = new int[3][3];
+        // Now this
+//        players[][] SuccessMoves = new players[3][3];
         int[][] Moves = new int[3][3];
 
         System.out.println(" 1,1 │ 1,2 │ 1,3 ");
@@ -103,7 +114,8 @@ public class TicTacToe {
         System.out.println("Enter Your Move in Given Coordinate Form :  ");
         int cord1 = Sc.nextInt();
         int cord2 = Sc.nextInt();
-        Board(Moves, cord1, cord2);
+
+        Board(SuccessMoves, cord1, cord2);
 
     }
 }
