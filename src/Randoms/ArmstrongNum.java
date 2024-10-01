adb612ac9f21f81a8f6c5bb0d30f5bbba01a354d
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2024-10-01 23:57:03 +0530

    Added Some Files

--- src/Randoms/ArmstrongNum.java
+++ src/Randoms/ArmstrongNum.java
@@ -6,7 +6,23 @@ public class ArmstrongNum {
     public static void main(String[] args) {
         Scanner Sc = new Scanner(System.in);
         int num = Sc.nextInt();
-        int rem = 0 ;
+        int rem;
+        int res = 0;
+        int ogNum = num;
+
+        while (ogNum != 0) {
+            // remainder contains the last digit
+            rem = ogNum % 10;
+
+            res += rem * rem * rem;
+
+            // removing last digit from the orignal number
+            ogNum /= 10;
+        }
+        if (res == num)
+            System.out.println(num + " is an Armstrong number.");
+        else
+            System.out.println(num + " is an not an Armstrong number.");
     }
 }
 /*
